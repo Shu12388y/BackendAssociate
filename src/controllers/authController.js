26c19c36842f6a2b6413d8e104a0840a8afc2083
Shub12388y <spl.sp999@gmail.com>
2024-10-30 00:50:10 +0530

    fix the error

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -178,8 +178,6 @@ const deleteUser = async (req, res) => {
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
-
-
         res.json({ message: 'User deleted successfully' });
     } catch (error) {
         res.status(500).json({ message: error.message });
